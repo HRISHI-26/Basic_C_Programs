d296cec8f7a7722f10a2aafbd1bcb4e9ecdd6bbf
HRISHI-26 <75836938+HRISHI-26@users.noreply.github.com>
2021-09-03 19:51:09 +0530

    Add files via upload

---
+++ HRISHI-26_Knowing_console_and_operators.c
@@ -0,0 +1,15 @@
+//Familiarization of Console and operators in C 
+/* Double slash is commenting a line for not excecuting remaining things in a line.
+Slash star can be used to comment a paragraph like this. */
+
+#include<stdio.h>       //including a library
+
+void main()     //including a function
+
+{
+
+    int a;      //Variable declaration
+    printf("Helloworld");       //Display a word
+    scanf("%d\n",&a);       //Reading an integer
+}
+//Stop
